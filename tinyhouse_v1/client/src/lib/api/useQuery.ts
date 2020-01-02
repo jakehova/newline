d08f3693fad88301b005439cec2ec9d722c035db
Jacob Cheriathundam <jacobcheriathundam@gmail.com>
2020-01-02 14:59:27 -0500

    added usequery

---
+++ tinyhouse_v1/client/src/lib/api/useQuery.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { server } from "./server";
+
+interface State<TData> {
+  data: TData | null;
+}
+
+export const useQuery = <TData = any>(query: string) => {
+  const [state, setState] = useState<State<TData>>({
+    data: null
+  });
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      console.log("getting data");
+      const { data } = await server.fetch<TData>({ query });
+      setState({ data });
+    };
+
+    fetchApi();
+  }, [query]);
+
+  return state;
+};
